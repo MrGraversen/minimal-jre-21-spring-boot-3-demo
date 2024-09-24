303612236940342185bd7430b9543cb7abe7ee48
Martin Graversen <martin@graversen.io>
2024-09-24 22:44:31 +0200

    :construction: Simple Spring Boot 3 + JRE 21 demo app

---
+++ spring-boot-jre21-demo-app/src/main/java/io/graversen/demo/SpringBootJre21DemoApp.java
@@ -0,0 +1,11 @@
+package io.graversen.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJre21DemoApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJre21DemoApp.class, args);
+    }
+}
